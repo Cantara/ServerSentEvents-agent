f5a854894131e4265b17eba96a4773a4e824412a
baardl <bard.lind@gmail.com>
2023-08-13 10:43:02 +0200

    Enable logging from cxf LogUtils

--- src/main/java/no/cantara/sse/SseClient.java
+++ src/main/java/no/cantara/sse/SseClient.java
@@ -19,6 +19,7 @@ public class SseClient {
     private static final Logger log = getLogger(SseClient.class);
 
     public static void main(String[] args) throws Exception {
+
         if (args.length != 2) {
             System.out.println("Usage: java -cp \"target/ServerSentEvents-0.1.0-SNAPSHOT.jar\" no.cantara.sse.SseClient <sseUrl> <bearerToken>");
             System.exit(1);
@@ -29,22 +30,29 @@ public class SseClient {
         URI sseUri = new URI(sseUrl);
 
         log.info("Subscribing to SSE events from {}", sseUrl);
-
         WebTarget target = ClientBuilder.newClient().target(sseUri);
         target.request(MediaType.SERVER_SENT_EVENTS).header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken);
 //        target.property("Authentication", "Bearer " + bearerToken);
         SseEventSource eventSource = SseEventSource.target(target).build();
         eventSource.register(event -> {
             log.info("Received Event. {}", event);
-//            System.out.println(event);
+            System.out.println(event);
             log.trace("Event Name: {}, Data: {}, Id: {}", event.getName(), event.readData(), event.getId());
 //            System.out.println(event.readData());
         });
 
-        int sec = 60;
-        log.info("Open connection for {} sec.", sec);
-        eventSource.open();
+        log.info("Opening target {}", target);
 
+        int sec = 3600;
+        log.info("Open connection for {} sec.", sec);
+        try {
+            eventSource.open();
+        } catch (Exception e) {
+            log.error("Error opening connection to {}", sseUrl, e);
+            System.exit(1);
+        }
+        Thread.sleep(1000);
+        log.info("Connection is open? {}", eventSource.isOpen());
         Thread.sleep(sec *1000);
 
         eventSource.close();
